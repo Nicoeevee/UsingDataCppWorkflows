a362de3c3db12f0193b01dc7db8cd6021d4e0309
Apo <nicoeevee@gmail.com>
2022-02-27 13:57:23 +0800

    Initial commit

---
+++ Source/UsingData/Actors/DataActorTable.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataActorTable.h"
+
+#if WITH_EDITOR
+void ADataActorTable::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (!bRunCode || ItemType == EItemType::None) { return; }
+
+	if (ItemDataTable)
+	{
+		// Get clean Row Name
+		const FName RowName = FName(UEnum::GetDisplayValueAsText(ItemType).ToString());
+		FItemInformationTable* Row = ItemDataTable->FindRow<FItemInformationTable>(RowName, "Item Details");
+		
+		if(Row)
+		{
+			GenericItemInfo.ItemType = Row->ItemType;
+			GenericItemInfo.Mesh = Row->Mesh;
+			GenericItemInfo.ItemCost = Row->ItemCost;
+			GenericItemInfo.ItemImage = Row->ItemImage;
+			GenericItemInfo.ItemInfo = Row->ItemInfo;
+			GenericItemInfo.ItemName = Row->ItemName;
+			GenericItemInfo.CurveFloat = Row->CurveFloat;
+			GenericItemInfo.RotationDuration = Row->RotationDuration;
+
+			Mesh->SetStaticMesh(GenericItemInfo.Mesh);
+		}
+	}
+	
+}
+#endif
